ce5b0497d3f2da5a547c20bb8ddcf970e3528836
Faruk Cesur <faruk-cesur@hotmail.com>
2024-12-14 14:22:19 +0300

    Updated Java Learning

--- JavaLearning/app/src/main/java/com/example/javalearning/Arrays.java
+++ JavaLearning/app/src/main/java/com/example/javalearning/Arrays.java
@@ -1,30 +1,77 @@
 package com.example.javalearning;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Arrays {
 
     public static void main(String[] args) {
 
-        String[] stringArray = new String[3];
-        stringArray[0] = "Faruk";
-        stringArray[1] = "Sena";
-        stringArray[2] = "Cesur";
+        //Array
 
-        System.out.println(stringArray[0]);
-        System.out.println(stringArray[1]);
-        System.out.println(stringArray[2]);
+        String[] myStringArray = new String[4];
 
-        ArrayList<String> arrayList = new ArrayList<>();
+        myStringArray[0] = "James";
+        myStringArray[1] = "Lars";
+        myStringArray[2] = "Kirk";
+        myStringArray[3] = "Rob";
 
-        arrayList.add("EVET");
-        arrayList.add("HAYIR");
-        arrayList.add(0,"YOK");
+        System.out.println(myStringArray[1]);
 
-        System.out.println(arrayList.get(0));
-        System.out.println(arrayList.get(1));
-        arrayList.clear();
-        System.out.println(arrayList.isEmpty());
+        int[] myIntegerArray = new int[4];
+        myIntegerArray[0] = 50;
+        myIntegerArray[1] = 60;
+        myIntegerArray[2] = 70;
+        myIntegerArray[3] = 80;
+
+        System.out.println(myIntegerArray[1]);
+
+        int[] myNumberArray = {1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.println(myNumberArray[2]);
+
+
+        //Lists
+
+        ArrayList<String> myMusicians = new ArrayList<String>();
+
+        myMusicians.add("James");
+        myMusicians.add("Lars");
+        myMusicians.add(1, "Kirk");
+        myMusicians.add("Rob");
+
+        System.out.println(myMusicians.get(0));
+        System.out.println(myMusicians.get(1));
+        System.out.println(myMusicians.get(2));
+        System.out.println(myMusicians.get(3));
+
+        System.out.println(myMusicians.size());
+
+        //Set
+
+        HashSet<String> mySet = new HashSet<String>();
+
+        mySet.add("James");
+        mySet.add("James");
+
+        System.out.println(mySet.size());
+
+        //HashMap
+
+        HashMap<String, String> myHashMap = new HashMap<String, String>();
+
+        myHashMap.put("name", "James");
+        myHashMap.put("instrument", "Guitar");
+
+        System.out.println(myHashMap.get("name"));
+        System.out.println(myHashMap.get("instrument"));
+
+
+        HashMap<String, Integer> mySecondMap = new HashMap<>();
+
+        mySecondMap.put("run", 100);
+        mySecondMap.put("basketball", 200);
+
+        System.out.println(mySecondMap.get("run"));
     }
 }
