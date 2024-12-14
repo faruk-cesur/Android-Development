ff224bd46d73cdc1f7afce1d3cea8e6a74f6c952
Faruk Cesur <faruk-cesur@hotmail.com>
2024-12-14 12:36:17 +0300

    Created JavaLearning Project

---
+++ JavaLearning/app/src/main/java/com/example/javalearning/First.java
@@ -0,0 +1,9 @@
+package com.example.javalearning;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(5*5);
+        System.out.println("Hello Faruk");
+    }
+}
