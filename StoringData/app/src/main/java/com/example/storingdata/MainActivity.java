54f24d75193fffed0242dc7dca72e47eaed9a12f
Faruk Cesur <faruk-cesur@hotmail.com>
2025-01-24 16:25:29 +0300

    Completed Storing Data Lesson

--- StoringData/app/src/main/java/com/example/storingdata/MainActivity.java
+++ StoringData/app/src/main/java/com/example/storingdata/MainActivity.java
@@ -1,24 +1,49 @@
 package com.example.storingdata;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText enterAgeEditText;
+    TextView yourAgeTextView;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        enterAgeEditText = findViewById(R.id.enterAgeEditText);
+        yourAgeTextView = findViewById(R.id.yourAgeTextView);
+        sharedPreferences = this.getSharedPreferences("com.example.storingdata", Context.MODE_PRIVATE);
+        int storedAge = sharedPreferences.getInt("storedAge",0);
+        if (storedAge == 0){
+            yourAgeTextView.setText("Your Age: ");
+        }
+        else {
+            yourAgeTextView.setText("Your Age: " + storedAge);
+        }
+    }
+
+    public void saveEnteredAge(View view) {
+        if (!enterAgeEditText.getText().toString().isEmpty()){
+            int userAge = Integer.parseInt(enterAgeEditText.getText().toString());
+            yourAgeTextView.setText("Your age: " + userAge);
+
+            sharedPreferences.edit().putInt("storedAge",userAge).apply();
+        }
+    }
+
+    public void deleteStoredAge(View view){
+        int storedData = sharedPreferences.getInt("storedAge",0);
+        if (storedData != 0){
+            sharedPreferences.edit().remove("storedAge").apply();
+            yourAgeTextView.setText("Your age: ");
+        }
     }
 }
