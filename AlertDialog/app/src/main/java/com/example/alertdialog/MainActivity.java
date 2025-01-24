7b9454e877c10ce45364eae306e19b58c50cc565
Faruk Cesur <faruk-cesur@hotmail.com>
2025-01-24 17:42:27 +0300

    Created Alert Dialog Project (In Progress)

---
+++ AlertDialog/app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.alertdialog;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void save(View view) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+
+        alert.setTitle("Save Data");
+        alert.setMessage("Do you really want to save?");
+
+        //alert.setNegativeButton(MainActivity.this, )
+
+    }
+}
