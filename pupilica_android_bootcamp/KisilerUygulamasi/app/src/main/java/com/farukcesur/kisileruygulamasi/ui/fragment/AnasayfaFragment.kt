8c91727ac8d3525a52f9e38386496da8ef1cce49
Faruk Cesur <faruk-cesur@hotmail.com>
2025-05-08 22:59:05 +0300

    Added Search View & CardView Design

--- pupilica_android_bootcamp/KisilerUygulamasi/app/src/main/java/com/farukcesur/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
+++ pupilica_android_bootcamp/KisilerUygulamasi/app/src/main/java/com/farukcesur/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
@@ -1,10 +1,12 @@
 package com.farukcesur.kisileruygulamasi.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.navigation.Navigation
 import com.farukcesur.kisileruygulamasi.R
 import com.farukcesur.kisileruygulamasi.data.entity.Kisiler
@@ -23,12 +25,22 @@ class AnasayfaFragment : Fragment() {
             Navigation.findNavController(it).navigate(R.id.kisiKayitGecis)
         }
 
-        binding.buttonDetay.setOnClickListener {
-            val x = Kisiler(1, "Ahmet", "1111")
-            val gecis = AnasayfaFragmentDirections.kisiDetayGecis(kisi = x)
-            Navigation.findNavController(it).navigate(gecis)
-        }
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String): Boolean { // Harf girdikçe ve sildikçe
+                ara(newText)
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean { // Ara butonuna basılınca
+                ara(query)
+                return true
+            }
+        })
 
         return binding.root
+    }
+
+    fun ara(aramaKelimesi:String){
+        Log.e("Kişi Ara", aramaKelimesi)
     }
 }
