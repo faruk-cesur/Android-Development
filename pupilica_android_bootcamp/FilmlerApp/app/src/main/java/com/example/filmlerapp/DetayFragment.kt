d81b30d2d8940ea02d068f8612b1f7a1a61b848d
Faruk Cesur <faruk-cesur@hotmail.com>
2025-05-11 18:41:56 +0300

    Completed Filmler App with Adapter

--- pupilica_android_bootcamp/FilmlerApp/app/src/main/java/com/example/filmlerapp/DetayFragment.kt
+++ pupilica_android_bootcamp/FilmlerApp/app/src/main/java/com/example/filmlerapp/DetayFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.filmlerapp.databinding.FragmentDetayBinding
 
 
@@ -16,7 +17,14 @@ class DetayFragment : Fragment() {
     ): View? {
         binding = FragmentDetayBinding.inflate(inflater, container, false)
 
+        val bundle: DetayFragmentArgs by navArgs()
+        val film = bundle.filmNesnesi
 
+        binding.toolbarDetay.title = film.ad
+
+        binding.tvFiyat.text = "${film.fiyat} ₺"
+
+        binding.ivFilm.setImageResource(resources.getIdentifier(film.resim, "drawable", requireContext().packageName))
 
         return binding.root
     }
