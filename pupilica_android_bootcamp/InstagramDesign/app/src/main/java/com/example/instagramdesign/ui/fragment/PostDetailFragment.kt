97cc0d24eaa4102538721d9ca0411f7ba485fbea
Faruk Cesur <faruk-cesur@hotmail.com>
2025-05-12 11:57:19 +0300

    Created Fragments

---
+++ pupilica_android_bootcamp/InstagramDesign/app/src/main/java/com/example/instagramdesign/ui/fragment/PostDetailFragment.kt
@@ -0,0 +1,18 @@
+package com.example.instagramdesign.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.instagramdesign.R
+
+class PostDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_post_detail, container, false)
+    }
+}
