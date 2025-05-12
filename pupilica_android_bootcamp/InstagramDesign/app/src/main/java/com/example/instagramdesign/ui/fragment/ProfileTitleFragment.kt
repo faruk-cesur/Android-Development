017e2153e099b105a9530fbd276c221b789f9aca
Faruk Cesur <faruk-cesur@hotmail.com>
2025-05-13 00:29:48 +0300

    Added Profile title Design

---
+++ pupilica_android_bootcamp/InstagramDesign/app/src/main/java/com/example/instagramdesign/ui/fragment/ProfileTitleFragment.kt
@@ -0,0 +1,18 @@
+package com.example.instagramdesign.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.instagramdesign.R
+
+class ProfileTitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile_title, container, false)
+    }
+}
