847b2eb6f76eb0680f52ff2106c3ad8cba517a3a
Faruk Cesur <faruk-cesur@hotmail.com>
2025-05-19 16:09:31 +0300

    Created ToDoRepository

---
+++ My-Projects/ToDoApp/app/src/main/java/com/farukcesur/todoapp/repository/TodoRepository.kt
@@ -0,0 +1,31 @@
+package com.farukcesur.todoapp.repository
+
+import androidx.lifecycle.LiveData
+import com.farukcesur.todoapp.data.TodoDao
+import com.farukcesur.todoapp.data.TodoEntity
+import javax.inject.Inject
+
+/*
+| Kod Parçası                          | Açıklama                                                           |
+| ------------------------------------ | ------------------------------------------------------------------ |
+| `@Inject constructor(...)`           | Hilt'in bu sınıfı otomatik olarak oluşturabilmesi için gereklidir. |
+| `insertTodo(...)`, `deleteTodo(...)` | DAO fonksiyonlarına erişimi sağlar.                                |
+| `getAllTodos()`                      | Live olarak veriyi takip etmemizi sağlar.                          |
+*/
+
+class TodoRepository @Inject constructor(
+    private val todoDao: TodoDao
+) {
+
+    suspend fun insertTodo(todo: TodoEntity) {
+        todoDao.insertTodo(todo)
+    }
+
+    suspend fun deleteTodo(todo: TodoEntity) {
+        todoDao.deleteTodo(todo)
+    }
+
+    fun getAllTodos(): LiveData<List<TodoEntity>> {
+        return todoDao.getAllTodos()
+    }
+}
