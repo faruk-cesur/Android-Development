85f2c9b7784ecc2dfa7f7b6040e643574a9a085e
Faruk Cesur <faruk-cesur@hotmail.com>
2025-05-22 04:58:39 +0300

    Updated Retrofit Version

--- My-Projects/OrderFoodApp/app/build.gradle.kts
+++ My-Projects/OrderFoodApp/app/build.gradle.kts
@@ -55,9 +55,9 @@ dependencies {
     kapt("com.google.dagger:hilt-android-compiler:2.56")
 
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.6.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.5.0")
 
     // Glide
     implementation ("com.github.bumptech.glide:glide:4.16.0")
