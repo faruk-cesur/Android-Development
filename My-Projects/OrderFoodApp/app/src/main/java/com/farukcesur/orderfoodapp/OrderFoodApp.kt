437155c975b6f821692e0381fd61b46785abaf0f
Faruk Cesur <faruk-cesur@hotmail.com>
2025-05-22 06:22:45 +0300

    Hilt Configuration (Dependency Injection)

---
+++ My-Projects/OrderFoodApp/app/src/main/java/com/farukcesur/orderfoodapp/OrderFoodApp.kt
@@ -0,0 +1,10 @@
+// OrderFoodApp.kt
+package com.farukcesur.orderfoodapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class OrderFoodApp : Application(){
+
+}
