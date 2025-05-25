ea9e49da345f193124374ad78597cf10ec2c8188
Faruk Cesur <faruk-cesur@hotmail.com>
2025-05-25 11:50:29 +0300

    Create AppModule.kt

---
+++ My-Projects/OrderFoodApp/app/src/main/java/com/farukcesur/orderfoodapp/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.farukcesur.orderfoodapp.di
+
+import com.farukcesur.orderfoodapp.data.remote.ApiClient
+import com.farukcesur.orderfoodapp.data.remote.FoodService
+import com.farukcesur.orderfoodapp.data.repository.FoodRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideFoodService(): FoodService {
+        return ApiClient.foodService
+    }
+
+    @Provides
+    @Singleton
+    fun provideFoodRepository(foodService: FoodService): FoodRepository {
+        return FoodRepository(foodService)
+    }
+}
