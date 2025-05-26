3fd5e0bf2343e20df8e352c62ba479c4d96bd2f0
Faruk Cesur <faruk-cesur@hotmail.com>
2025-05-26 09:25:20 +0300

    Fixed App Crash & Added Internet Connection Permission

--- My-Projects/OrderFoodApp/app/src/main/java/com/farukcesur/orderfoodapp/MainActivity.kt
+++ My-Projects/OrderFoodApp/app/src/main/java/com/farukcesur/orderfoodapp/MainActivity.kt
@@ -1,31 +1,21 @@
+// GÜNCELLENMİŞ MainActivity.kt
+
 package com.farukcesur.orderfoodapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.farukcesur.orderfoodapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // ActionBar ile Back düğmesini çalıştırmak için
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        NavigationUI.setupActionBarWithNavController(this, navController)
     }
-
-//    // Back tuşuna navController ile dönmesini sağla
-//    override fun onSupportNavigateUp(): Boolean {
-//        val navController = findNavController(R.id.nav_host_fragment)
-//        return navController.navigateUp() || super.onSupportNavigateUp()
-//    }
 }
