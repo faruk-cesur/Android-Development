ff6cc49bf68617507c3cf1c18239af52916718b9
Faruk Cesur <faruk-cesur@hotmail.com>
2025-10-15 17:46:33 +0300

    Created Timer App Project

---
+++ Learning-Projects/TimerApp02/app/src/main/java/com/example/timerapp_02/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.timerapp_02
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TimerApp()
+        }
+    }
+}
+
+@Composable
+fun TimerApp() {
+    var count by remember { mutableStateOf(0) }
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = "Sayaç: $count", style = MaterialTheme.typography.headlineMedium)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {count++}){
+            Text("Arttır")
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(onClick = { count-- }){
+            Text("Azalt")
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(onClick = {count = 0}){
+            Text("Sıfırla")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TimerAppPreview(){
+    TimerApp()
+}
+
