8f2067cb97b87a75001e84da3b99a755849bf600
Faruk Cesur <faruk-cesur@hotmail.com>
2026-02-20 10:38:19 +0300

    Updated codebase to MVVM

--- Learning-Projects/LearningJetpackCompose/app/src/main/java/com/example/learningjetpackcompose/MainActivity.kt
+++ Learning-Projects/LearningJetpackCompose/app/src/main/java/com/example/learningjetpackcompose/MainActivity.kt
@@ -7,65 +7,84 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            GameScreen()
+            MainGameScreen()
+        }
+    }
+}
+
+class GameViewModel: ViewModel(){
+    var currentHealth by mutableStateOf(100)
+        private set
+
+    val isCharacterDead : Boolean
+        get() = currentHealth <= 0
+    
+    val maxHealth: Int
+        get() = 100
+
+    fun takeDamage(damageAmount:Int){
+        if (!isCharacterDead) currentHealth -= damageAmount
+    }
+
+    fun heal(healAmount: Int){
+        if (!isCharacterDead) {
+            currentHealth = (currentHealth + healAmount).coerceAtMost(maxHealth)
         }
     }
 }
 
 @Composable
-fun GameScreen(){
-    var health by remember {mutableStateOf(100)}
-
+fun StatelessGameUI(health: Int, onDamageClick:()-> Unit){
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier
-        .fillMaxSize()
-        .background(color = Color.LightGray)) {
-        Text(text= if(health>0)"Health: $health" else "Character is dead!",
-            modifier = Modifier
-                .padding(16.dp),
-            fontWeight = FontWeight.Bold,
-            fontSize = 32.sp)
+            .fillMaxSize()
+            .background(color = Color.LightGray)
+            .padding(16.dp)) {
+        Text(
+            text = if(health>0)"Health: $health" else "Character is DEAD!",
+            modifier = Modifier.padding(16.dp),
+            fontSize = 32.sp,
+            color = if(health > 0) Color.Black else Color.Red)
 
-        Button(onClick = {health-= 10}, enabled = health > 0){
-            Text("Damage",modifier = Modifier
-                .padding(8.dp),
-                fontSize = 24.sp
-                )
+        Button(onClick = onDamageClick, enabled = health > 0) {
+            Text("DAMAGE", fontSize = 24.sp)
         }
     }
 }
 
-@Preview
 @Composable
-fun PreviewGameScreen(){
-    GameScreen()
+fun MainGameScreen(){
+    val viewModel: GameViewModel = viewModel()
+
+    StatelessGameUI(viewModel.currentHealth, onDamageClick = {viewModel.takeDamage(10)})
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMainGameScreen(){
+    MainGameScreen()
 }
